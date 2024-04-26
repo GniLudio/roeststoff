55936af917c5d87e6f1abcff24ba6d3164863f6c
GniLudio <ferdinand.rohlfing@googlemail.com>
2024-04-26 20:31:56 +0200

    Initial setup\n\nSetup for Webpack with TypeScript

---
+++ dist/main.js
@@ -0,0 +1,10 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!**********************!*\
+  !*** ./src/index.ts ***!
+  \**********************/
+console.log("index.ts loaded");
+
+/******/ })()
+;
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoibWFpbi5qcyIsIm1hcHBpbmdzIjoiOzs7OztBQUFBLE9BQU8sQ0FBQyxHQUFHLENBQUMsaUJBQWlCLENBQUMsQ0FBQyIsInNvdXJjZXMiOlsid2VicGFjazovL3JvZXN0c3RvZmYvLi9zcmMvaW5kZXgudHMiXSwic291cmNlc0NvbnRlbnQiOlsiY29uc29sZS5sb2coXCJpbmRleC50cyBsb2FkZWRcIik7XHJcbiJdLCJuYW1lcyI6W10sInNvdXJjZVJvb3QiOiIifQ==
