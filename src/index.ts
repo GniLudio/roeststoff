2d21116a6899d36db9107d55eaf6418c58806b1b
GniLudio <ferdinand.rohlfing@googlemail.com>
2024-07-15 01:01:31 +0200

    Add episode 37 and fetch rss from url

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,4 @@
 console.log("index.ts loaded");
-import episodesRaw from '../data/episodes.xml';
 import peopleRaw from '../data/people.xml';
 import drinksRaw from '../data/drinks.xml';
 import boestOfsRaw from '../data/boestofs.xml';
@@ -11,36 +10,44 @@ import * as parser from './parser';
 import * as cards from './cards';
 import { compareEpisodeID, compareEpisode, compareEpisodeIDs, compareIsHost } from './utils';
 
-// PARSING
-console.log("PARSING")
-const episodes: Episode[] = parser.parseXML(episodesRaw, ['rss', 'channel'], 'item', parser.parseEpisode);
-const people: Person[] = parser.parseXML(peopleRaw, ['root'], 'person', parser.parsePerson);
-const drinks: Drink[] = parser.parseXML(drinksRaw, ['root'], 'drink', parser.parseDrink);
-const boestOfs: BoestOf[] = parser.parseXML(boestOfsRaw, ['root'], 'boestof', parser.parseBoestOf);
-const restaurants: Restaurant[] = parser.parseXML(restaurantsRaw, ['root'], 'restaurant', parser.parseRestaurant);
-const glossary: GlossaryEntry[] = parser.parseXML(glossaryRaw, ['root'], 'entry', parser.parseGlossaryEntry);
-const misc: MiscEntry[] = parser.parseXML(miscRaw, ['root'], 'entry', parser.parseMiscEntry);
-const sayings: TextWithTimestamp[] = parser.parseXML(sayingsRaw, ['root'], 'saying', parser.parseTextWithTimestamp);
-const allContent: AllContent = {episodes, people, drinks, boestOfs, restaurants, glossary, sayings, misc};
+const RSS_URL = "https://feeds.megaphone.fm/TWG3193347111";
 
-// SORTING
-console.log("SORTING");
-episodes.sort(compareEpisode).reverse();
-people.sort((a,b) => -compareEpisodeIDs(a.appearances, b.appearances, allContent.episodes)).sort((a,b) => -compareIsHost(a, b));
-drinks.sort((a,b) => -compareEpisodeIDs(a.appearances, b.appearances, allContent.episodes));
-boestOfs.sort((a,b) => -compareEpisodeID(a, b, allContent.episodes));
-restaurants.sort((a,b) => -compareEpisodeIDs(a.appearances, b.appearances, allContent.episodes));
-glossary.sort((a,b) => a.name.localeCompare(b.name));
-sayings.sort((a,b) => -compareEpisodeID(a, b, episodes));
-misc.sort((a,b) => -compareEpisodeID(a, b, allContent.episodes));
+(async () => {
+    console.log("FETCHING RSS");
+    const episodesRaw = await (await fetch(RSS_URL)).text();
+    console.log(episodesRaw);
 
-// CREATE AND INSERT HTML ELEMENTS
-console.log("CREATING");
-cards.createCards('episodes_content', episodes, cards.getEpisodeCardInfo, allContent);
-cards.createCards('people_content', people, cards.getPersonCardInfo, allContent);
-cards.createCards('drinks_content', drinks, cards.getDrinkCardInfo, allContent);
-cards.createCards('boestof_content', boestOfs, cards.getBoestOfCardInfo, allContent);
-cards.createCards('restaurants_content', restaurants, cards.getRestaurantCardInfo, allContent);
-cards.createCards('glossary_content', glossary, cards.getGlossaryEntryCardInfo, allContent);
-cards.createCards('sayings_content', sayings, cards.getSayingCardInfo, allContent);
-cards.createCards('misc_content', misc, cards.getMiscEntryCardInfo, allContent);
+    // PARSING
+    console.log("PARSING")
+    const episodes: Episode[] = parser.parseXML(episodesRaw, ['rss', 'channel'], 'item', parser.parseEpisode);
+    const people: Person[] = parser.parseXML(peopleRaw, ['root'], 'person', parser.parsePerson);
+    const drinks: Drink[] = parser.parseXML(drinksRaw, ['root'], 'drink', parser.parseDrink);
+    const boestOfs: BoestOf[] = parser.parseXML(boestOfsRaw, ['root'], 'boestof', parser.parseBoestOf);
+    const restaurants: Restaurant[] = parser.parseXML(restaurantsRaw, ['root'], 'restaurant', parser.parseRestaurant);
+    const glossary: GlossaryEntry[] = parser.parseXML(glossaryRaw, ['root'], 'entry', parser.parseGlossaryEntry);
+    const misc: MiscEntry[] = parser.parseXML(miscRaw, ['root'], 'entry', parser.parseMiscEntry);
+    const sayings: TextWithTimestamp[] = parser.parseXML(sayingsRaw, ['root'], 'saying', parser.parseTextWithTimestamp);
+    const allContent: AllContent = {episodes, people, drinks, boestOfs, restaurants, glossary, sayings, misc};
+
+    // SORTING
+    console.log("SORTING");
+    episodes.sort(compareEpisode).reverse();
+    people.sort((a,b) => -compareEpisodeIDs(a.appearances, b.appearances, allContent.episodes)).sort((a,b) => -compareIsHost(a, b));
+    drinks.sort((a,b) => -compareEpisodeIDs(a.appearances, b.appearances, allContent.episodes));
+    boestOfs.sort((a,b) => -compareEpisodeID(a, b, allContent.episodes));
+    restaurants.sort((a,b) => -compareEpisodeIDs(a.appearances, b.appearances, allContent.episodes));
+    glossary.sort((a,b) => a.name.localeCompare(b.name));
+    sayings.sort((a,b) => -compareEpisodeID(a, b, episodes));
+    misc.sort((a,b) => -compareEpisodeID(a, b, allContent.episodes));
+
+    // CREATE AND INSERT HTML ELEMENTS
+    console.log("CREATING");
+    cards.createCards('episodes_content', episodes, cards.getEpisodeCardInfo, allContent);
+    cards.createCards('people_content', people, cards.getPersonCardInfo, allContent);
+    cards.createCards('drinks_content', drinks, cards.getDrinkCardInfo, allContent);
+    cards.createCards('boestof_content', boestOfs, cards.getBoestOfCardInfo, allContent);
+    cards.createCards('restaurants_content', restaurants, cards.getRestaurantCardInfo, allContent);
+    cards.createCards('glossary_content', glossary, cards.getGlossaryEntryCardInfo, allContent);
+    cards.createCards('sayings_content', sayings, cards.getSayingCardInfo, allContent);
+    cards.createCards('misc_content', misc, cards.getMiscEntryCardInfo, allContent);
+})();
